9e3dbc6f6e97832f49b4475e44fb519b07a361ea
Francis Wibisono <code@franciswibisono.com>
2025-04-22 16:32:33 +0700

    feat: test be

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<script>
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+    
+    onMount(async  () => {
+        const res = await (await fetch('http://localhost:3000/health-check')).text()
+        console.log(res);
+    });
+    
+    $: console.log('Current page:', $page);
+</script>
